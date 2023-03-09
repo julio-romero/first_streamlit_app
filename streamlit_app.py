2799ee82d4ea4231d4c644161d046f1aa6fd401a
maaanuelromero <manuelerreo@gmail.com>
2023-03-09 08:19:10 -0600

    addded

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,4 +49,6 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit list contains:")
 streamlit.dataframe(my_data_rows)
 
+fruit_selected = streamlit.text_input("What fruit would you like to add?")
+streamlit.text("Thanks for adding"+fruit_selected)
 
